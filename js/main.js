e61b81d2a7bdbc9d083ceb8013c8d3eae5e213ab
Kodux78k <koduxlive@gmail.com>
2026-02-04 01:33:20 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
 console.log("Infodose conectado", {"ts":1770173968742,"id":"265a3f64-86bf-4ec6-afe2-313cb90b4fc8","meta":{"app":"generated.app"}});
-import "./modules/inline-di.js";
+import "./modules/inline-0.js";
